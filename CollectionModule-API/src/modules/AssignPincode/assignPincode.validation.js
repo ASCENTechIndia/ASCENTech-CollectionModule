5fba3a54e8239cb45017fd2e14df94d32df12ddd
KalyaniAscentech <kalyani.bharsat@ascentechindia.com>
2026-04-13 18:40:16 +0530

    assign pincode

---
+++ CollectionModule-API/src/modules/AssignPincode/assignPincode.validation.js
@@ -0,0 +1,16 @@
+const { z } = require('zod');
+
+const nullableString = z.string().trim().optional().nullable();
+
+const userFindSchema = z.object({
+userId: z.coerce.number().int().min(1)
+})
+
+const pinCodeAssignSchema = z.object({
+username: z.number().min(1, "UserId is required"),
+  pincode_str: z.string()
+    .regex(/^(\d{6})(~\d{6})*$/, "Invalid pincode format")
+});
+
+module.exports = {
+  userFindSchema, pinCodeAssignSchema}
