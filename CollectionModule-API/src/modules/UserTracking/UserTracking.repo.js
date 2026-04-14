8c1a3b7a6597ba5bfc5fa82a6d891cad2456d04a
KalyaniAscentech <kalyani.bharsat@ascentechindia.com>
2026-04-14 16:52:05 +0530

    user tracking

--- CollectionModule-API/src/modules/UserTracking/UserTracking.repo.js
+++ CollectionModule-API/src/modules/UserTracking/UserTracking.repo.js
@@ -13,7 +13,9 @@ function normalizeNullable(value) {
 
 async function getUserLocationTracking(userId, cDate) {
   let sql = `
-    SELECT userid, location, TO_CHAR(cdate, 'DD/MM/YYYY') AS cdate FROM etech.aoup_userLocation WHERE userid = :userId AND TRUNC(cdate) = TO_DATE(:cDate, 'YYYY-MM-DD')
+    SELECT userid, location, TO_CHAR(cdate, 'DD/MM/YYYY') AS cdate FROM etech.aoup_userLocation WHERE 
+    userid = :userId AND TRUNC(cdate) = TO_DATE(:cDate, 'YYYY-MM-DD')
+    order by ROWID DESC FETCH FIRST 1 ROW ONLY
   `;
 
   const binds = { userId: userId, cDate: cDate };
