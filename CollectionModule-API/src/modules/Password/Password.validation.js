1f09d830b686c86d73389226aff2a0613e2b3e95
KalyaniAscentech <kalyani.bharsat@ascentechindia.com>
2026-04-14 13:17:19 +0530

    change password

--- CollectionModule-API/src/modules/Password/Password.validation.js
+++ CollectionModule-API/src/modules/Password/Password.validation.js
@@ -6,6 +6,17 @@ const resetPasswordSchema = z.object({
   userId: z.string().min(1, "UserId is required"),
 });
 
+const changePasswordSchema = z.object({
+  userId: z.string().min(1, "UserId is required"),
+
+  oldPassword: z.string().min(1, "Old password is required"),
+
+  newPassword: z.string()
+    .min(6, "Minimum 6 characters required")
+    .refine((val) => !/[@#$%&~\-\^\*]/.test(val), {
+      message: "Password should not contain @,#,$,%,&,~,-,^,* special characters",
+    }),
+});
 
 module.exports = {
-  resetPasswordSchema}
+  resetPasswordSchema, changePasswordSchema}
