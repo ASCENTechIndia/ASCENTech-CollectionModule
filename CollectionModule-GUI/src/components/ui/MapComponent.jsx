a1af02a04aa2acd7b6f45a0998b57deb820d672f
aniket-pandit-15 <aniket.pandit@ascentechindia.com>
2026-04-15 10:20:36 +0530

    Changes in map component

--- CollectionModule-GUI/src/components/ui/MapComponent.jsx
+++ CollectionModule-GUI/src/components/ui/MapComponent.jsx
@@ -1,28 +1,3 @@
-// import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
-
-// const MapComponent = ({ lat, lng }) => {
-//   const mapContainerStyle = { width: "100%", height: "400px" };
-//   const center = { lat, lng };
-//   console.log("cener ", center);
-//   const apiKey = import.meta.env.VITE_GOOGLE_MAP_PUBLIC_KEY;
-
-//   return (
-//     <LoadScript googleMapsApiKey={apiKey}>
-//       <GoogleMap
-//         mapContainerStyle={mapContainerStyle}
-//         center={center}
-//         zoom={15}
-//       >
-//         <Marker position={center} />
-
-       
-//       </GoogleMap>
-//     </LoadScript>
-//   );
-// };
-
-// export default MapComponent;
-
 import { APIProvider, Map, Marker } from "@vis.gl/react-google-maps";
 
 const MapComponent = ({ lat, lng }) => {
