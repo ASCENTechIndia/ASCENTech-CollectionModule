74565982069f2fd4c694d39816d636fc76d1de9d
Pingcodeping <Pingcodeping@gmail.com>
2026-04-17 15:22:11 +0530

    Integrate API - FrmContractAllocation

--- CollectionModule-GUI/src/pages/Admin/FrmContractAllocation.jsx
+++ CollectionModule-GUI/src/pages/Admin/FrmContractAllocation.jsx
@@ -11,7 +11,42 @@ import { AlertCircle } from 'lucide-react';
 const FrmContractAllocation = () => {
     const { user } = useAuth();
     const [isChecked, setIsChecked] = useState(false);
+    const [count, setCount] = useState("");
+    const [message, setMessage] = useState("");
     const [showMessage, setShowMessage] = useState(false);
+    
+
+    const fetchRecordCount = async () => {
+        try {
+            const response = await apiClient.get("/admin/getAccCounts", {});
+
+            if (response.data.success) {
+                setCount(response.data.data[0].NULLFOSCOUNT);
+            }
+        } catch (error) {
+            console.error(error?.response);
+        }
+    }
+
+    const handleAllocateAccounts = async () => {
+        try {
+            const response = await apiClient.post("/admin/allocateAccount", {});
+
+            if (response.data.success && response.data.data.success) {
+                setMessage(response.data.data.message);
+                setShowMessage(true);
+            }
+        } catch (error) {
+            console.error(error?.response);
+        }
+    }
+
+
+    useEffect(() => {
+        fetchRecordCount();
+    }, []);
+
+
     return (
         <div className="min-h-screen bg-gray-50">
             <div className="max-w-5xl mx-auto px-4 py-6 space-y-6">
@@ -26,17 +61,24 @@ const FrmContractAllocation = () => {
                             onChange={setIsChecked}
                         />
                     </div>
-                    {}
+                    {showMessage && (
+                        <div>
+                            <span className="text-green-600">{message}</span>
+                        </div>
+                    )}
                     <div>
                         <button
-                            type="submit"
+                            type="button"
                             className="px-8 py-2.5 bg-primary-600 hover:bg-primary-700 text-white font-medium rounded-lg transition-colors focus:outline-none focus:ring-2 focus:ring-primary-500 focus:ring-offset-2"
+                            onClick={() => {
+                                handleAllocateAccounts();
+                            }}
                         >
                             Allocate Accounts
                         </button>
                     </div>
                     <div>
-                        <span className='text-sm font-semibold'>No New records for allocation</span>
+                        {count === 0 ? <span className='text-sm font-semibold'>No New records for allocation</span> : <span className="text-sm font-semibold">{`Count: ${count}`}</span> }
                     </div>
                 </div>
             </div>
